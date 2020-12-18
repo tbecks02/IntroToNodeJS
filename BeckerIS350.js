730143f703568c53bfe86fd532eac2a7bceb3e62
tbecks02 <becket36@uwosh.edu>
2020-12-18 04:54:53 +0000

    Added package.jason, PORT changes to web server, procfile

--- BeckerIS350.js
+++ BeckerIS350.js
@@ -1,29 +1,38 @@
 // creating your first web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require ('http');
+var fs = require ('fs');
+var url = require ('url');
+const PORT = process.env.PORT || 5000; 
 
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
-	var filename = "." + q.pathname;
+	varfilename = "." + q.pathname;
 
-if (filename == './') {
-	filename = './index';
-}
+	if (filename == './') {
+		filename = './index';
+	}
 
-filename = filename + ".html";
-console.log('loading:...' + filename);
+	filename = filename + ".html";
+	console.log('loading:...' + filename);
 
-fs.readFile(filename, function(err, data) {
-	if (err){
-		res.writeHead(404, {'Content-Type': 'text/html'});
-		return res.end("404: Page Not Found");
-}
+	fs.readFile(filename, funciton(err, data) {
+		if (err){
+			res.writeHead(404, {'Content-Type': 'tet/html'});
+			return res.end("404: Page Not Found");
+		}
 
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.write(data);
-return res.end();
-});
+		res.writeHead(200, {'Content-Type': 'text/hrml'});
+		res.write(data);
+		return.res.end();
+	});
+}).listen(PORT);
 
-}).listen(8080);
-console.log("Server Listening on Port 8080...");
+console.log("Serve listening on assigned PORT...")
+
+//CREATE TABLE `ecommerce`.`product` ( `ProductID` INT NOT NULL ,
+// `ImageURL` TEXT NOT NULL , 
+//`Price` DECIMAL NOT NULL , 
+//`Description` VARCHAR(500) NOT NULL ,
+// `InventoryAmount` INT NOT NULL , 
+//`DateCreated` DATETIME NOT NULL , 
+//`DateUpdated` DATETIME NOT NULL ) ENGINE = MyISAM; 
